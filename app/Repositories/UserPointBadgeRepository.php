2cf7f372e386e95e2b00aea9505f2c5365b615b3
Roberto Peres <roberto.peres@begore.com.br>
2022-06-13 20:27:04 -0400

    [master] 🚜 Melhorias rank

--- app/Repositories/UserPointBadgeRepository.php
+++ app/Repositories/UserPointBadgeRepository.php
@@ -26,43 +26,44 @@ class UserPointBadgeRepository extends RepositoryAbstract
     {
         return DB::connection()->select('
                 SELECT tb_rank.user_id,
-                       tb_rank.user_name,
-                       tb_rank.user_google_avatar,
-                       tb_rank.badge_id,
-                       tb_rank.badge_icon,
-                       badge_name,
-                       badge_type_id,
-                       badge_type_description,
-                       badge_classification_id,
-                       badge_classification_description,
-                       MIN(tb_rank.badge_value) as value,
-                       tb_rank.total
-                FROM (
-                         SELECT tb_users.*,
-                                badge.id                         as badge_id,
-                                badge.name                       as badge_name,
-                                badge.value                      as badge_value,
-                                badge.icon                       as badge_icon,
-                                user_badge.id                    as user_badge_id,
-                                badge_type.description as badge_type_description,
-                                badge.badge_classification_id    as badge_classification_id,
-                                badge_classification.description as badge_classification_description
-                         FROM (SELECT user.id                     as user_id,
-                                      user.name                   as user_name,
-                                      user.google_avatar          as user_google_avatar,
-                                      SUM(user_point_badge.value) as total,
-                                      user_point_badge.badge_type_id
-                               FROM user
-                                        INNER JOIN user_point_badge ON (user_point_badge.user_id = user.id)
-                               WHERE user.active = true
-                               GROUP BY user.id, user_point_badge.badge_type_id) as tb_users
-                                  INNER JOIN badge ON (badge.badge_type_id = tb_users.badge_type_id)
-                                  INNER JOIN badge_classification ON (badge_classification.id = badge.badge_classification_id)
-                                   INNER JOIN badge_type ON (badge_type.id = badge.badge_type_id)
-                                  LEFT JOIN user_badge ON (user_badge.user_id = tb_users.user_id AND user_badge.badge_id = badge.id)
-                         ORDER BY tb_users.user_id, badge.value) as tb_rank
-                WHERE tb_rank.user_badge_id IS NULL
-                GROUP BY tb_rank.user_id, tb_rank.badge_type_id ORDER BY badge_classification_id DESC;
+                   tb_rank.user_name,
+                   tb_rank.user_google_avatar,
+                   tb_rank.badge_id,
+                   tb_rank.badge_icon,
+                   badge_name,
+                   badge_type_id,
+                   badge_type_description,
+                   badge_classification_id,
+                   badge_classification_description,
+                   MIN(tb_rank.badge_value) as value,
+                   tb_rank.total
+            FROM (SELECT tb_users.*,
+                         badge.id                         as badge_id,
+                         badge.name                       as badge_name,
+                         badge.value                      as badge_value,
+                         badge.icon                       as badge_icon,
+                         user_badge.id                    as user_badge_id,
+                         badge_type.description           as badge_type_description,
+                         badge.badge_classification_id    as badge_classification_id,
+                         badge_classification.description as badge_classification_description
+                  FROM (SELECT user.id                     as user_id,
+                               user.name                   as user_name,
+                               user.google_avatar          as user_google_avatar,
+                               SUM(user_point_badge.value) as total,
+                               user_point_badge.badge_type_id,
+                               user.admission_date         as admission_date
+                        FROM user
+                                 INNER JOIN user_point_badge ON (user_point_badge.user_id = user.id)
+                        WHERE user.active = true
+                        GROUP BY user.id, user_point_badge.badge_type_id) as tb_users
+                           INNER JOIN badge ON (badge.badge_type_id = tb_users.badge_type_id)
+                           INNER JOIN badge_classification ON (badge_classification.id = badge.badge_classification_id)
+                           INNER JOIN badge_type ON (badge_type.id = badge.badge_type_id)
+                           LEFT JOIN user_badge ON (user_badge.user_id = tb_users.user_id AND user_badge.badge_id = badge.id)
+                  ORDER BY badge.value, tb_users.admission_date) as tb_rank
+            WHERE tb_rank.user_badge_id IS NULL
+            GROUP BY tb_rank.user_id, tb_rank.badge_type_id
+            ORDER BY tb_rank.badge_classification_id DESC;
         ');
     }
 
