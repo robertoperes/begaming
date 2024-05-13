91bd248aa07ae86f383a9dc3b21b5ae8fdb115c9
Robert Peres <roberto.peres@before.com.br>
2024-05-13 08:28:54 -0400

    [master] - 📝 melhorias home

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -46,7 +46,7 @@ class Kernel extends ConsoleKernel
         $schedule->command(CacheDashboardCommand::class)->cron('55 * * * *')
             ->appendOutputTo(storage_path() . '/logs/schedule.log');
 
-        $schedule->command(CollectStravaActivitiesCommand::class)->cron('0 */2 * * *')
+        $schedule->command(CollectStravaActivitiesCommand::class)->cron('*/5 * * * *')
             ->appendOutputTo(storage_path() . '/logs/schedule.log');
 
         $schedule->command(CompanyTimePointCommand::class)->cron('0 5 * * *')
