6cfd9c9d20a8638634fc05437d847a682c59c87f
Robert Peres <roberto.peres@before.com.br>
2024-05-29 15:45:16 -0400

    [master] - 📝 melhorias

--- resources/js/components/Dashboard/RankBadgeUser.vue
+++ resources/js/components/Dashboard/RankBadgeUser.vue
@@ -5,21 +5,25 @@
       <div class="card-header" title="Ranking de badges">
         Ranking
       </div>
-      <div class="card-body p-1">
+      <div class="card-body p-2">
         <div v-for="(user, index) in this.badgeUsersRank" :key="'rank-' + index" :title="user.name"
-             class="row m-2 shadow-sm">
-          <div class="col-2 p-0 text-center">
+             class="row">
+          <div class="col-2 text-center">
             <img v-if="(index+1) === 1" src="/images/icons/medal-1st.svg" width="30" alt=""/>
             <img v-else-if="(index+1) === 2" src="/images/icons/medal-2nd.svg" width="30" alt=""/>
             <img v-else-if="(index+1) === 3" src="/images/icons/medal-3rd.svg" width="30" alt=""/>
             <span v-else style="font-size: 10px">#{{ index + 1 }}</span>
           </div>
-          <div class="col-2 p-0">
+          <div class="col-2">
             <img :src="user.google_avatar || '/images/icons/profile.png'" class="rounded-circle" width="35" height="35"
                  alt=""/>
           </div>
-          <div class="col-6 p-0">{{ firstName(user.name) }}</div>
-          <div class="col-2 p-0 text-center"><strong>{{ user.total }}</strong></div>
+          <div class="col-6">
+            {{ firstName(user.name) }}
+          </div>
+          <div class="col-2 text-center">
+            <strong>{{ user.total }}</strong>
+          </div>
         </div>
       </div>
     </div>
